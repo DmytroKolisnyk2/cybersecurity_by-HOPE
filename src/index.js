0cc9443a14b6e93abbb38d03e9439650811a6339
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-10-18 23:07:50 +0300

    add burger and menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './sass/main.scss';
 import 'material-design-icons/iconfont/material-icons.css';
+import { openBar } from './js/openBar';
+
+document.querySelector('.header__burger').addEventListener('click', openBar);
