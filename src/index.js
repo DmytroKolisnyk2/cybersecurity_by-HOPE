2a7d5d74e54c1704db890e8a2f9b687ca524a77c
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-10-23 18:43:06 +0300

    edit site in same style

--- src/index.js
+++ src/index.js
@@ -4,3 +4,28 @@ import { openBar,closeOverlayBar } from './js/openBar';
 
 document.querySelector('.header__burger').addEventListener('click', openBar);
 document.querySelector('.aside__overlay').addEventListener('click', closeOverlayBar);
+
+const video = document.querySelector('.main__video')
+
+function checkScroll() {
+  console.log(1)
+  video.play();
+  setTimeout(() => {
+    video.pause();
+  }, 200)
+
+}
+
+window.addEventListener('scroll', checkScroll);
+
+// video.pause();
+
+
+// window.onscroll = function () {
+//   video.pause();
+// };
+
+
+// setInterval(function () {
+//   video.currentTime = window.pageYOffset / 2230;
+// }, 10);
