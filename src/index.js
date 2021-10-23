8c176468338c7aaa353ba6ecae2c7d9b0dcb5e57
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-10-24 00:19:14 +0300

    add wwowjs

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 import './sass/main.scss';
 import 'material-design-icons/iconfont/material-icons.css';
-import { openBar,closeOverlayBar } from './js/openBar';
+import { openBar, closeOverlayBar } from './js/openBar';
+import throttle from 'lodash.throttle';
+import WOW from 'wow.js/dist/wow';
+// import 'animate.css/animate.min.css';
+import 'wow.js/css/libs/animate.css';
+
+
 
 document.querySelector('.header__burger').addEventListener('click', openBar);
 document.querySelector('.aside__overlay').addEventListener('click', closeOverlayBar);
 
-const video = document.querySelector('.main__video')
+const video = document.querySelector('.main__video');
 
-function checkScroll() {
-  console.log(1)
+const checkScroll = () => {
   video.play();
   setTimeout(() => {
     video.pause();
-  }, 200)
+  }, 300);
+};
 
-}
+window.addEventListener('scroll', throttle(checkScroll, 150));
 
-window.addEventListener('scroll', checkScroll);
+new WOW().init();
 
 // video.pause();
-
 
 // window.onscroll = function () {
 //   video.pause();
 // };
-
 
 // setInterval(function () {
 //   video.currentTime = window.pageYOffset / 2230;
