9a38b651988554cc4c87ca4eaa1e5e1e3cea9005
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-10-24 13:38:59 +0300

    fix bug after merge

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -1293,7 +1293,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43803" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33787" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
