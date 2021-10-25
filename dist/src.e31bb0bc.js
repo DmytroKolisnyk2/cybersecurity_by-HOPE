799047d45327e93882468dc6bce9710ea833c2e1
Igoryakib <Igoryakib@gmail.com>
2021-10-25 16:39:17 +0300

    fix mobile version

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -1345,7 +1345,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58641" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50988" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
