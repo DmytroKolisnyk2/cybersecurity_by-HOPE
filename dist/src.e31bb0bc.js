f42d23c2a1df33cd120e17a54377de91a27370ec
Max Popsuy <maxim09p@gmail.com>
2021-10-28 11:55:29 +0300

    changed text in 2 sections

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/material-design-icons/iconfont/material-icons.css":[function(require,module,exports) {
+},{"./..\\images\\igor_yakib.jpg":[["igor_yakib.15a67f86.jpg","images/igor_yakib.jpg"],"images/igor_yakib.jpg"],"./..\\images\\max_popsuy.jpg":[["max_popsuy.30c2a167.jpg","images/max_popsuy.jpg"],"images/max_popsuy.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/material-design-icons/iconfont/material-icons.css":[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
@@ -1327,7 +1327,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53670" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54351" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
