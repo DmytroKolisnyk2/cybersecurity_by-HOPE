3f51148aef38a1b50ccab6f8bbb23a409659b3b4
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-10-29 11:54:58 +0300

    fix bugs

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ import './js/checkArrow';
 import baguetteBox from 'baguettebox.js';
 import 'baguettebox.js/dist/baguetteBox.min.css';
 
-baguetteBox.run('.section__photo-wrapper--js-gallery',{
+baguetteBox.run('.section__photo-wrapper--js-gallery', {
   noScrollbars: true,
-})
+});
 
 document.querySelector('.header__burger').addEventListener('click', openBar);
 document.querySelector('.aside__overlay').addEventListener('click', closeOverlayBar);
@@ -32,14 +32,15 @@ window.addEventListener('scroll', throttle(checkScroll, 150));
 new WOW().init();
 
 const spy = new Gumshoe('#menu .list__link');
-console.log(document.querySelectorAll('#menu .list__link'))
+console.log(document.querySelectorAll('#menu .list__link'));
 
 window.addEventListener('scroll', throttle(hideHeader, 100));
 const options = {
   root: null,
   rootMargin: '0px',
-  threshold: 0.5
-}
+  threshold: window.screen.width <= 768 ? 0.2 : 0.5,
+};
+console.log(options.threshold);
 const target = document.querySelector('.cyberSecurity__footer');
 const onEntry = (entries, observer) => {
   entries.forEach(entry => {
@@ -47,35 +48,39 @@ const onEntry = (entries, observer) => {
     if (entry.isIntersecting) {
       // console.log(target)
       document.querySelectorAll('.title1').forEach(item => item.classList.add('title--animation'));
-        observer.unobserve(target);
+      observer.unobserve(target);
     }
-})
+  });
 };
 
 const observerFooter = new IntersectionObserver(onEntry, options);
 observerFooter.observe(target);
 
-
 const onEntryBorder = (entries, observer) => {
   entries.forEach(entry => {
     const target = entry.target;
-    if(entry.isIntersecting) {
-      if(target === document.querySelector('.final')) {
+    if (entry.isIntersecting) {
+      if (target === document.querySelector('.final')) {
         target.classList.add('appearBorder--specialWidth');
         setTimeout(() => {
           target.classList.add('appearBorder--specialHeight');
-        }, 1400)
+        }, 1400);
       } else {
         target.classList.add('appearBorder');
       }
     }
     // observer.unobserve(target);
-  })
+  });
 };
 
 const arrayTargets = [...document.querySelector('.main').children];
-arrayTargets.push(document.querySelector('.introduction__border'), document.querySelector('.main__section--before'), document.querySelector('.main__section--after'), document.querySelector('.block__line'));
+arrayTargets.push(
+  document.querySelector('.introduction__border'),
+  document.querySelector('.main__section--before'),
+  document.querySelector('.main__section--after'),
+  document.querySelector('.block__line'),
+);
 const observerBorder = new IntersectionObserver(onEntryBorder, options);
 arrayTargets.forEach(item => {
-  observerBorder.observe(item)
+  observerBorder.observe(item);
 });
