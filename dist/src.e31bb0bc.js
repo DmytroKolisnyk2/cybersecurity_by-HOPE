c57aead6e51d7a9115346ffcab6576e52a8cefca
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-10-30 18:55:56 +0300

    edit main__arrow

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -1416,7 +1416,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39043" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38009" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
