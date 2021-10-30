465b0e3636c5f5ed2aa8fd9e8dc9e5d01e8cb900
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-10-31 00:08:12 +0300

    add console section

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import 'wow.js/css/libs/animate.css';
 import Gumshoe from 'gumshoejs';
 import { hideHeader } from './js/hideHeader';
 import './js/checkArrow';
-
+import { info } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 import baguetteBox from 'baguettebox.js';
 import 'baguettebox.js/dist/baguetteBox.min.css';
 
@@ -32,7 +34,31 @@ window.addEventListener('scroll', throttle(checkScroll, 150));
 new WOW().init();
 
 const spy = new Gumshoe('#menu .list__link');
-console.log(document.querySelectorAll('#menu .list__link'));
+
+const checkWrittenInput = () => {
+  const inputValue = document.querySelector('.section__input').value;
+  console.log(inputValue)
+  if (inputValue) {
+    return `<span class="section__headline">Ось що ви вводили:</span> ${inputValue}`
+  }
+  else {
+    return '<span class="section__headline">Нажаль ви нічого не ввели</span> '
+  }
+};
+
+window.sayHello = () => {
+  if (!document.querySelector('.successful-text')) {
+    document.querySelector('#console-text').insertAdjacentHTML('beforeend', `<span class="section__headline section__headline--main successful-text">І який же результат?</span><br>
+      ${checkWrittenInput()} <br><br>
+      Таким чином навіть ввід даних у поле із <span>type="password"</span> не заважає зловмиснику отримати дані із інпута. <br>
+      <br> Тому не вводьте незнайомі вам команди у консоль, оскільки виконана функція могла так само надіслати ваші дані прямо в руки до шахрая.`);
+    info({ text: 'Ви виконали усе вірно', delay: 3000 })
+  }
+  else {
+    info({ text: 'Ви вже виконували команду', delay: 3000 })
+
+  }
+};
 
 window.addEventListener('scroll', throttle(hideHeader, 100));
 const options = {
@@ -40,13 +66,12 @@ const options = {
   rootMargin: '0px',
   threshold: window.screen.width <= 768 ? 0.1 : 0.5,
 };
-console.log(options.threshold);
+
 const target = document.querySelector('.cyberSecurity__footer');
 const onEntry = (entries, observer) => {
   entries.forEach(entry => {
     const target = entry.target;
     if (entry.isIntersecting) {
-      // console.log(target)
       document.querySelectorAll('.title1').forEach(item => item.classList.add('title--animation'));
       observer.unobserve(target);
     }
@@ -69,7 +94,6 @@ const onEntryBorder = (entries, observer) => {
         target.classList.add('appearBorder');
       }
     }
-    // observer.unobserve(target);
   });
 };
 
@@ -87,3 +111,14 @@ arrayTargets.forEach(item => {
 [...document.querySelectorAll('a')].forEach(element =>
   element.setAttribute('rel', 'noreferrer noopener nofollow'),
 );
+
+const copyBtnRef = document.querySelector('.copy-console-text-js');
+
+copyBtnRef.addEventListener('click', () => {
+  const textareaRef = document.querySelector('.section__textarea');
+  textareaRef.removeAttribute('disabled');
+  textareaRef.select();
+  document.execCommand('copy');
+  textareaRef.setAttribute('disabled', 'true');
+  info({ text: 'Команда була скопійована', delay: 2000 });
+});
